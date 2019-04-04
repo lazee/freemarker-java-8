98d83b7bcc48fe2619234ceabc4a2b75c18027c6
Fritz Lumnitz <fritz@lumoos.de>
2019-04-04 20:41:57 +0200

    Added support to retrieve builtin DateTimeFormatter

--- src/main/java/no/api/freemarker/java8/time/ExtFormatStyle.java
+++ src/main/java/no/api/freemarker/java8/time/ExtFormatStyle.java
@@ -1,25 +1,8 @@
 package no.api.freemarker.java8.time;
 
-import java.time.format.FormatStyle;
+import java.time.format.DateTimeFormatter;
 
-public enum ExtFormatStyle {
-    LONG_DATE(true, false, FormatStyle.LONG),
-    LONG_DATETIME(true, true, FormatStyle.LONG),
-    LONG_TIME(false, true, FormatStyle.LONG),
-    MEDIUM_DATE(true, false, FormatStyle.MEDIUM),
-    MEDIUM_DATETIME(true, true, FormatStyle.MEDIUM),
-    MEDIUM_TIME(false, true, FormatStyle.MEDIUM),
-    SHORT_DATE(true, false, FormatStyle.SHORT),
-    SHORT_DATETIME(true, true, FormatStyle.SHORT),
-    SHORT_TIME(false, true, FormatStyle.SHORT);
+public interface ExtFormatStyle {
 
-    final boolean withDate;
-    final boolean withTime;
-    final FormatStyle javaFormatStyle;
-
-    ExtFormatStyle(boolean withDate, boolean withTime, FormatStyle formatStyle) {
-        this.withDate = withDate;
-        this.withTime = withTime;
-        this.javaFormatStyle = formatStyle;
-    }
+	DateTimeFormatter getFormatter();
 }
